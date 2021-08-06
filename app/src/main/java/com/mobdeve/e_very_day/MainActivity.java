a2452e16fbcdd690d25123a5f710f0bc1e3546c0
Amos-Cacha <amos_cacha@dlsu.edu.ph>
2021-08-06 16:17:36 +0800

    Blank app

---
+++ app/src/main/java/com/mobdeve/e_very_day/MainActivity.java
@@ -0,0 +1,14 @@
+package com.mobdeve.e_very_day;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
