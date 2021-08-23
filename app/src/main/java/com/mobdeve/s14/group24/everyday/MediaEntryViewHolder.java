4bd650e8834f5312dd081859ecb21d27b3060657
JanjanC <janjan_chua@dlsu.edu.ph>
2021-08-24 00:34:17 +0800

    Create MediaEntryViewHolder.java

---
+++ app/src/main/java/com/mobdeve/s14/group24/everyday/MediaEntryViewHolder.java
@@ -0,0 +1,40 @@
+package com.mobdeve.s14.group24.everyday;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.jetbrains.annotations.NotNull;
+
+public class MediaEntryViewHolder extends RecyclerView.ViewHolder {
+
+    private ImageView ivMoodRating;
+    private TextView tvDate;
+    private ImageView ivImage;
+
+    public MediaEntryViewHolder(@NonNull @NotNull View itemView) {
+        super(itemView);
+
+        ivMoodRating = itemView.findViewById(R.id.iv_item_media_mood_rating);
+        tvDate = itemView.findViewById(R.id.tv_item_media_date);
+        ivImage = itemView.findViewById(R.id.iv_item_media_image);
+    }
+
+    public void setIvMoodRating(int moodRating) {
+        ivMoodRating.setImageResource(moodRating);
+    }
+
+    public void setTvDate(String date) {
+        tvDate.setText(date);
+    }
+
+    public void setIvImage(int imageID) {
+        ivImage.setImageResource(imageID);
+    }
+}
+
+
+
