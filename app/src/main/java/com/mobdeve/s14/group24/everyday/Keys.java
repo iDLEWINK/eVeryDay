3ee9d3e55170c8bf705c20260f3ac56f19a67218
Amos-Cacha <amos_cacha@dlsu.edu.ph>
2021-08-24 14:34:00 +0800

    basic stuff, trying to push to make gradle stop being weird

---
+++ app/src/main/java/com/mobdeve/s14/group24/everyday/Keys.java
@@ -0,0 +1,4 @@
+package com.mobdeve.s14.group24.everyday;
+
+public enum Keys {
+}
---
+++ app/src/main/java/com/mobdeve/s14/group24/everyday/ViewEntryActivity.java
@@ -0,0 +1,14 @@
+package com.mobdeve.s14.group24.everyday;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewEntryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_entry);
+    }
+}
