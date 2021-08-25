f84aca34dec74b3e61a0bc6c82dadacf444e48cd
JanjanC <janjan_chua@dlsu.edu.ph>
2021-08-25 20:17:18 +0800

    Change Activity name

--- app/src/main/java/com/mobdeve/s14/group24/everyday/ViewMediaEntryActivity.java
+++ app/src/main/java/com/mobdeve/s14/group24/everyday/ViewMediaEntryActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ViewEntryActivity extends AppCompatActivity {
+public class ViewMediaEntryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_view_entry);
+        setContentView(R.layout.activity_view_media_entry);
     }
 }
