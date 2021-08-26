193199b20b4c7cbe84063df3f27397c01605fad6
JanjanC <janjan_chua@dlsu.edu.ph>
2021-08-26 16:09:40 +0800

    Repositioned some functions

--- app/src/main/java/com/mobdeve/s14/group24/everyday/MainActivity.java
+++ app/src/main/java/com/mobdeve/s14/group24/everyday/MainActivity.java
@@ -1,35 +1,26 @@
 package com.mobdeve.s14.group24.everyday;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.FileProvider;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-import java.io.File;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView rvGallery;
     private RecyclerView.Adapter mediaEntryAdapter;
 
-    private DatabaseHelper dbh;
+    private DatabaseHelper databaseHelper;
     private DataHelper dataHelper;
     private ArrayList<MediaEntry> mediaEntries;
 
@@ -41,10 +32,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        dbh = new DatabaseHelper(this);
+        databaseHelper = new DatabaseHelper(this);
+
         dataHelper = new DataHelper(MainActivity.this);
         dataHelper.resetData();
         dataHelper.initializeData();
+
         initRecyclerView();
         initFabCamera();
     }
@@ -70,29 +63,32 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                if (dbh.getRowByDate(new CustomDate()) != null) {
+                if (databaseHelper.getRowByDate(new CustomDate()) != null) {
                     Toast.makeText(getApplicationContext(), "You have already made an entry for today", Toast.LENGTH_LONG).show();
-                    return;
+                } else {
+                    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                    CameraHelper cameraHelper = new CameraHelper(getApplicationContext());
+                    startActivityForResult(cameraHelper.makeIntent(), CameraHelper.REQUEST_IMAGE_CAPTURE);
+                    currentPhotoPath = cameraHelper.getCurrentPhotoPath();
                 }
-
-                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
-                CameraHelper cameraHelper = new CameraHelper(getApplicationContext());
-                startActivityForResult(cameraHelper.makeIntent(), CameraHelper.REQUEST_IMAGE_CAPTURE);
-                currentPhotoPath = cameraHelper.getCurrentPhotoPath();
-                dbh.addEntry(currentPhotoPath);
             }
         });
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
         if (requestCode == CameraHelper.REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
+            databaseHelper.addEntry(currentPhotoPath);
+
+
+
             Intent intent = new Intent(getApplicationContext(), ViewMediaEntryActivity.class);
             intent.putExtra(Keys.KEY_IMAGE_PATH.name(), currentPhotoPath);
             intent.putExtra(Keys.KEY_DATE.name(), new CustomDate().toStringFull());
+
             startActivity(intent);
         }
     }
